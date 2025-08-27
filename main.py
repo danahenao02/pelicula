5da9d1343e7cf0d55c4571e945f82688ef36633c
Danahenao02 <226599953+danahenao02@users.noreply.github.com>
2025-08-27 18:06:11 -0500

    rapido

--- main.py
+++ main.py
@@ -16,22 +16,9 @@ import sys
 favoritas = []  
 historial = []  
 
-def main():
-    """Función principal del programa"""
-    print("Iniciando Netflix Console...")
-    
-    print("Programa iniciado correctamente")
-
-
-if __name__ == "__main__":
-    main()
-
 """
 NETFLIX CONSOLE - Carga de datos JSON
 """
-
-import json
-import os
 
 def cargar_peliculas():
     """
@@ -40,7 +27,7 @@ def cargar_peliculas():
     """
     try:
         
-        if not os.path.exists('peliculas json'):
+        if not os.path.exists('peliculas.json'):
             print(" Error: No se encontró el archivo 'peliculas.json'")
             print(" Asegúrate de que esté en la misma carpeta que main.py")
             return None
@@ -95,25 +82,6 @@ def mostrar_estadisticas_carga(peliculas):
     if mejor_pelicula:
         print(f" Mejor calificada: {mejor_pelicula['titulo']} ({mejor_rating}/10)")
 
-def main():
-    """Función principal del programa """
-    print("  NETFLIX CONSOLE - Cargando datos...")
-    print("=" * 50)
-    
-
-    print(" Cargando películas desde peliculas.json...")
-    peliculas = cargar_peliculas()
-    
-    if peliculas:
-        print("¡Películas cargadas exitosamente!")
-        mostrar_estadisticas_carga(peliculas)
-    else:
-        print(" Error: No se pudieron cargar las películas")
-        print(" Revisa que el archivo peliculas.json esté en la carpeta correcta")
-        return
-    
-    print("\n Sistema listo para usar")
-
 def limpiar_pantalla():
      """ limpiar pantalla de la consola"""
      os.system('cls' if os.name == 'nt' else 'clear')
@@ -151,23 +119,50 @@ def obtener_opcion_usuario():
         
             else:
                 print("opcion no valida. usa numeros del 0 al 9.")
+                pass
 
         except KeyboardInterrupt:
             print("\n\¡hasta luego!")
             sys.exit(0)
         except Exception as e:
+            
             print(f"error: {e}")
 
 def pausar():
     """pausa el programa hasta que el usuario presione enter"""
     input("\npresiona enter para continuar...")
 
-    def main():
-        """funcion principal con bucle de menu interactivo"""
+def main():
 
-        #cargar datos
-        print("no se puede consultar peliculas")
+    """Función principal del programa"""
+    print("Iniciando Netflix Console...")
+    
+    print("Programa iniciado correctamente")
+
+    """Función principal del programa """
+    print("  NETFLIX CONSOLE - Cargando datos...")
+    print("=" * 50)
+    
+
+    print(" Cargando películas desde peliculas.json...")
+    peliculas = cargar_peliculas()
+    
+    if peliculas:
+        print("¡Películas cargadas exitosamente!")
+        mostrar_estadisticas_carga(peliculas)
+    else:
+        print(" Error: No se pudieron cargar las películas")
+        print(" Revisa que el archivo peliculas.json esté en la carpeta correcta")
         return
+    
+    print("\n Sistema listo para usar")
+    """funcion principal con bucle de menu interactivo"""
+
+    print("iniciando netflix console")
+    peliculas = cargar_peliculas()
+    if not peliculas:
+        print ("nos e puede continuar sin las peliculas")
+
     print("sistema cargando correctamente")
     pausar()
 
@@ -175,6 +170,7 @@ def pausar():
     while True:
         limpiar_pantalla()
         mostrar_header()
+        
         mostrar_menu_principal()
 
         opcion = obtener_opcion_usuario
@@ -183,6 +179,54 @@ def pausar():
         if opcion == "0":
             limpiar_pantalla()
             print("¡gracias por usar Netflix console!")
+            print("¡que disfrutes viendo peliculas!")
             print("¡hasta la proxima!")
             break
+        elif opcion == "1":
+            print("\n has elegido la pelicula de accion")
+            print("funcion en desarrollo")
+            pausar()
 
+        elif opcion == "2":
+            print("\n has elegido la pelicula de comedia")
+            print("funcion en desarrollo")
+            pausar()
+
+        elif opcion == "3":
+            print("\n has elegido la pelicula de comedia")
+            print("funcion en desarrollo")
+            pausar()
+
+        elif opcion == "4":
+            print("\n has elegido la pelicula de terror")
+            print("funcion en desarrollo")
+            pausar()
+
+        elif opcion == "5":
+            print("\n has elegido la pelicula de ciencia")
+            print("funcion en desarrollo")
+            pausar()
+
+        elif opcion == "6":
+            print("\n has elegido buscar pelicula")
+            print("funcion en desarrollo")
+            pausar()
+        
+        elif opcion == "7":
+            print("\n has elegido top 10")
+            print("funcion en desarrollo")
+            pausar()
+
+        elif opcion == "8":
+            print("\n has elegido mis favoritas")
+            print("funcion en desarrollo")
+            pausar()
+
+        elif opcion == "9":
+            limpiar_pantalla()
+            mostrar_header()
+            mostrar_estadisticas_carga()
+            pausar()
+            
+if __name__ == "__main__":
+    main()
